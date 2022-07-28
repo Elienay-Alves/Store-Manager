077ad9210e87837b681f7da2d19d618ce501cd93
Elienay-Alves <elienay8486@gmail.com>
2022-07-28 16:42:27 -0300

    adicionado requisito 2

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,16 +5,16 @@ const productController = {
 
   async list(_req, res) {
     const items = await productService.list();
-    res.status(200).json(items);
+    return res.status(200).json(items);
   },
 
   async listId(req, res) {
     try {
       const { id } = req.params;
       const item = await productService.listId(id);
-      res.status(200).json(item);
+      return res.status(200).json(item);
     } catch (err) {
-      res.status(404).json({ message: err.message });
+      return res.status(404).json({ message: err.message });
     }
   },
 };
